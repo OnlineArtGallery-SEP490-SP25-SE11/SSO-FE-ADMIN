2882c411345b81f6c932b42da8fa8bee3abaf89b
hydra07 <abcxyzkhong@gmail.com>
2024-12-31 22:24:22 +0700

    🙌:update sidebar admin

---
+++ src/app/providers.tsx
@@ -0,0 +1,20 @@
+'use client';
+import QueryWrapper from '@/components/query-wrapper';
+import { ThemeProvider } from '@/components/theme-wrapper';
+import { Toaster } from '@/components/ui/toaster';
+import { TooltipProvider } from '@/components/ui/tooltip';
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryWrapper>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <TooltipProvider>{children}</TooltipProvider>
+        <Toaster />
+      </ThemeProvider>
+    </QueryWrapper>
+  );
+}
